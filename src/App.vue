1e88c94037b0d4b7f70d9b35a4daa351ba7c9ef7
Andrejs Grocs <andrey.detochkin@gmail.com>
2022-04-18 10:17:27 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <p>{{name}} - {{age}}</p>
+    <button @click='changeName("Taschibana Ukyo")'>change name</button>
+    <button @click='changeAge(31)'>change age</button>
+
+
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+
+let age1: string | number = 45   // We are not able to use it inside the data() object
+
+import { defineComponent, reactive, toRefs } from "vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: {},
+  setup(){
+    const state = reactive({
+      name: 'Link',
+      age: 18 as string | number
+    })
+    return{...toRefs(state)}
+
+
+  },
+/*   data(){
+    return{
+      name:'Link',
+      age: 25 as number | string     // The way how we choose type of data in objects and in this case data() object
+    }
+  }, */
+  methods:{
+    changeName(name: string){
+       this.name =  name
+       return name
+    },
+    changeAge(age: string|number){
+      this.age = age
+       return age                            // Ts automatically returns :void but if we will make a return then we will receive the information about returning data tye on hovering on the function name.
+    }
+
   }
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
